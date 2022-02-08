f9db122b32d454052abec27133a12b9ba8009cf8
Eugeniy <Nesterovich2797@gmail.com>
2022-02-08 11:45:11 +0300

    Create ru.js
    Translation into Russian

---
+++ src/translations/ru.js
@@ -0,0 +1,71 @@
+{
+    "editor": {
+        "card": {
+            "generic": {
+                "vertical": "Вертикальный вид",
+                "multiline_secondary": "Многострочная доп информация",
+                "hide_state": "Спрятать состояние",
+                "state": "Состояние",
+                "icon_color": "Цвет иконки",
+                "color_values": {
+                    "default": "Стандартный цвет"
+                }
+            },
+            "light": {
+                "show_brightness_control": "Управление яркостью",
+                "show_color_temp_control": "Управление цветовой температурой"
+            },
+            "fan": {
+                "animate_icon_active": "Анимировать иконку во время активности",
+                "show_percentage_control": "Управление в процентах",
+                "show_oscillate_control": "Управление вращением"
+            },
+            "cover": {
+                "show_buttons_control": "Управление кнопками",
+                "show_position_control": "Управление положением"
+            },
+            "person": {
+                "use_entity_picture": "Использовтаь изображение объекта?"
+            },
+            "alarm_control_panel": {
+                "displayed_states": "Отображаемые состояния"
+            },
+            "entity": {
+                "primary_info": "Основная информация",
+                "secondary_info": "Дополнительная информация",
+                "info_values": {
+                    "default": "Стандартные настройки",
+                    "name": "Название",
+                    "state": "Состояние",
+                    "last-changed": "Последнее изменение",
+                    "last-updated": "Последнее обновление",
+                    "none": "Ничего"
+                }
+            },
+            "template": {
+                "primary": "Основная информация",
+                "secondary": "Дополнительная информация"
+            },
+            "title": {
+                "title": "Заголовок",
+                "subtitle": "Подзаголовок"
+            }
+        },
+        "chip": {
+            "chip-picker": {
+                "chips": "Chips",
+                "details": "Отображать подробную информацию при клике на кнопку редактирования",
+                "add": "Добавить chip",
+                "edit": "Редактировать",
+                "clear": "Очистить"
+            },
+            "weather": {
+                "show_conditions": "Условия?",
+                "show_temperature": "Температура?"
+            },
+            "template": {
+                "content": "Содержание"
+            }
+        }
+    }
+}
