787b36faf7e6b4097eeacc76975c2361a91066aa
Anders Lundgren <78078136+a-n-lundgren@users.noreply.github.com>
2022-02-21 23:57:14 +0100

    Initial Swedish translation (#151)
    * Create se.json
    
    Initial Swedish translation
    
    * Rename se.json to sv.json
    
    Renaming of the file to comply with naming conventions.
    
    * Swedish translations improvements
    
    Shortened some sentences for better contextual fit.
    
    * Added sv to localize.ts
    
    Added the Swedish translation to localize.ts

--- src/localize.ts
+++ src/localize.ts
@@ -6,6 +6,7 @@ import * as en from "./translations/en.json";
 import * as fr from "./translations/fr.json";
 import * as de from "./translations/de.json";
 import * as pt_BR from "./translations/pt-BR.json";
+import * as sv from "./translations/sv.json";
 import * as zh_Hans from "./translations/zh-Hans.json";
 
 const languages: Record<string, unknown> = {
@@ -13,6 +14,7 @@ const languages: Record<string, unknown> = {
     fr,
     de,
     "pt-BR": pt_BR,
+    sv,
     "zh-Hans": zh_Hans,
 };
 
