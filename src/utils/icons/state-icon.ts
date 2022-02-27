c957c441656a7c04ef62de65e779e9ccc3016fc8
Paul Bottein <paul.bottein@gmail.com>
2022-02-27 11:05:54 +0100

    fix(icon): use entity icon attributes if defined

--- src/utils/icons/state-icon.ts
+++ src/utils/icons/state-icon.ts
@@ -3,9 +3,13 @@ import { HassEntity } from "home-assistant-js-websocket";
 import { domainIcon } from "./domain-icon";
 
 export function stateIcon(entity: HassEntity): string {
+ ​   ​const state = entity.state;
+    
+    if​ ​(​state​.​attributes​.​icon​)​ ​{ 
+ ​       return​ ​state​.​attributes​.​icon​; 
+ ​  ​ }
+
     const domain = computeDomain(entity.entity_id);
-
-    const state = entity.state;
-
+ 
     return domainIcon(domain, entity, state);
 }
