354839f91fccbf1668585b803562e0ad0317f8b3
piitaya <paul.bottein@gmail.com>
2022-03-04 22:48:21 +0100

    refactor: migrate template card to ha-form

--- src/cards/template-card/template-card-editor.ts
+++ src/cards/template-card/template-card-editor.ts
@@ -1,22 +1,35 @@
-import {
-    computeRTLDirection,
-    fireEvent,
-    HomeAssistant,
-    LovelaceCardEditor,
-} from "custom-card-helpers";
+import { fireEvent, HomeAssistant, LovelaceCardEditor } from "custom-card-helpers";
 import { CSSResultGroup, html, LitElement, TemplateResult } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { assert } from "superstruct";
 import setupCustomlocalize from "../../localize";
-import "../../shared/editor/layout-picker";
-import "../../shared/form/mushroom-textarea";
 import { configElementStyle } from "../../utils/editor-styles";
+import { GENERIC_FIELDS } from "../../utils/form/fields";
+import { HaFormSchema } from "../../utils/form/ha-form";
 import { loadHaComponents } from "../../utils/loader";
-import { EditorTarget } from "../../utils/lovelace/editor/types";
 import { TEMPLATE_CARD_EDITOR_NAME } from "./const";
 import { TemplateCardConfig, templateCardConfigStruct } from "./template-card-config";
 
-const actions = ["toggle", "more-info", "navigate", "url", "call-service", "none"];
+const TEMPLATE_FIELDS = ["primary", "secondary", "multiline_secondary"];
+
+const SCHEMA: HaFormSchema[] = [
+    { name: "entity", selector: { text: {} } },
+    { name: "icon", selector: { text: { multiline: true } } },
+    { name: "icon_color", selector: { text: { multiline: true } } },
+    { name: "primary", selector: { text: { multiline: true } } },
+    { name: "secondary", selector: { text: { multiline: true } } },
+    {
+        type: "grid",
+        name: "",
+        schema: [
+            { name: "layout", selector: { "mush-layout": {} } },
+            { name: "multiline_secondary", selector: { boolean: {} } },
+        ],
+    },
+    { name: "tap_action", selector: { "mush-action": {} } },
+    { name: "hold_action", selector: { "mush-action": {} } },
+    { name: "double_tap_action", selector: { "mush-action": {} } },
+];
 
 @customElement(TEMPLATE_CARD_EDITOR_NAME)
 export class TemplateCardEditor extends LitElement implements LovelaceCardEditor {
@@ -34,179 +47,41 @@ export class TemplateCardEditor extends LitElement implements LovelaceCardEditor
         this._config = config;
     }
 
+    private _computeLabelCallback = (schema: HaFormSchema) => {
+        const customLocalize = setupCustomlocalize(this.hass!);
+
+        if (schema.name === "entity") {
+            return `${this.hass!.localize(
+                "ui.panel.lovelace.editor.card.generic.entity"
+            )} (${customLocalize("editor.card.template.entity_extra")})`;
+        }
+        if (GENERIC_FIELDS.includes(schema.name)) {
+            return customLocalize(`editor.card.generic.${schema.name}`);
+        }
+        if (TEMPLATE_FIELDS.includes(schema.name)) {
+            return customLocalize(`editor.card.template.${schema.name}`);
+        }
+        return this.hass!.localize(`ui.panel.lovelace.editor.card.generic.${schema.name}`);
+    };
+
     protected render(): TemplateResult {
         if (!this.hass || !this._config) {
             return html``;
         }
 
-        const dir = computeRTLDirection(this.hass);
-        const customLocalize = setupCustomlocalize(this.hass);
-
         return html`
-            <div class="card-config">
-                <ha-entity-picker
-                    .label="${this.hass.localize(
-                        "ui.panel.lovelace.editor.card.generic.entity"
-                    )} (${customLocalize("editor.card.template.entity_extra")})"
-                    .hass=${this.hass}
-                    .value=${this._config.entity}
-                    .configValue=${"entity"}
-                    @value-changed=${this._valueChanged}
-                    allow-custom-entity
-                ></ha-entity-picker
-                ><mushroom-textarea
-                    .label="${this.hass.localize(
-                        "ui.panel.lovelace.editor.card.generic.icon"
-                    )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                    .value=${this._config.icon ?? ""}
-                    .configValue=${"icon"}
-                    @keydown=${this._ignoreKeydown}
-                    @input=${this._valueChanged}
-                    dir="ltr"
-                    autogrow
-                    autocapitalize="none"
-                    autocomplete="off"
-                    spellcheck="false"
-                ></mushroom-textarea>
-                <mushroom-textarea
-                    .label="${customLocalize(
-                        "editor.card.generic.icon_color"
-                    )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                    .value=${this._config.icon_color ?? ""}
-                    .configValue=${"icon_color"}
-                    @keydown=${this._ignoreKeydown}
-                    @input=${this._valueChanged}
-                    dir="ltr"
-                    autogrow
-                    autocapitalize="none"
-                    autocomplete="off"
-                    spellcheck="false"
-                ></mushroom-textarea>
-                <mushroom-textarea
-                    .label="${customLocalize("editor.card.template.primary")} (${this.hass.localize(
-                        "ui.panel.lovelace.editor.card.config.optional"
-                    )})"
-                    .value=${this._config.primary ?? ""}
-                    .configValue=${"primary"}
-                    @keydown=${this._ignoreKeydown}
-                    @input=${this._valueChanged}
-                    dir="ltr"
-                    autogrow
-                    autocapitalize="none"
-                    autocomplete="off"
-                    spellcheck="false"
-                ></mushroom-textarea>
-                <mushroom-textarea
-                    .label="${customLocalize(
-                        "editor.card.template.secondary"
-                    )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                    .value=${this._config.secondary ?? ""}
-                    .configValue=${"secondary"}
-                    @keydown=${this._ignoreKeydown}
-                    @input=${this._valueChanged}
-                    dir="ltr"
-                    autogrow
-                    autocapitalize="none"
-                    autocomplete="off"
-                    spellcheck="false"
-                ></mushroom-textarea>
-                <div class="side-by-side">
-                    <mushroom-layout-picker
-                        .label="${customLocalize(
-                            "editor.card.generic.layout"
-                        )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                        .hass=${this.hass}
-                        .value=${this._config.layout}
-                        .configValue=${"layout"}
-                        @value-changed=${this._valueChanged}
-                    >
-                    </mushroom-layout-picker>
-                    <ha-formfield
-                        .label=${customLocalize("editor.card.template.multiline_secondary")}
-                        .dir=${dir}
-                    >
-                        <ha-switch
-                            .checked=${!!this._config.multiline_secondary}
-                            .configValue=${"multiline_secondary"}
-                            @change=${this._valueChanged}
-                        ></ha-switch>
-                    </ha-formfield>
-                </div>
-                <div class="side-by-side">
-                    <hui-action-editor
-                        .label="${this.hass.localize(
-                            "ui.panel.lovelace.editor.card.generic.tap_action"
-                        )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                        .hass=${this.hass}
-                        .config=${this._config.tap_action}
-                        .actions=${actions}
-                        .configValue=${"tap_action"}
-                        .tooltipText=${this.hass.localize(
-                            "ui.panel.lovelace.editor.card.button.default_action_help"
-                        )}
-                        @value-changed=${this._valueChanged}
-                    ></hui-action-editor>
-                    <hui-action-editor
-                        .label="${this.hass.localize(
-                            "ui.panel.lovelace.editor.card.generic.hold_action"
-                        )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                        .hass=${this.hass}
-                        .config=${this._config.hold_action}
-                        .actions=${actions}
-                        .configValue=${"hold_action"}
-                        .tooltipText=${this.hass.localize(
-                            "ui.panel.lovelace.editor.card.button.default_action_help"
-                        )}
-                        @value-changed=${this._valueChanged}
-                    ></hui-action-editor>
-                </div>
-                <div class="side-by-side">
-                    <hui-action-editor
-                        .label="${this.hass.localize(
-                            "ui.panel.lovelace.editor.card.generic.double_tap_action"
-                        )} (${this.hass.localize("ui.panel.lovelace.editor.card.config.optional")})"
-                        .hass=${this.hass}
-                        .config=${this._config.double_tap_action}
-                        .actions=${actions}
-                        .configValue=${"double_tap_action"}
-                        .tooltipText=${this.hass.localize(
-                            "ui.panel.lovelace.editor.card.button.default_action_help"
-                        )}
-                        @value-changed=${this._valueChanged}
-                    ></hui-action-editor>
-                </div>
-            </div>
+            <ha-form
+                .hass=${this.hass}
+                .data=${this._config}
+                .schema=${SCHEMA}
+                .computeLabel=${this._computeLabelCallback}
+                @value-changed=${this._valueChanged}
+            ></ha-form>
         `;
     }
 
-    private _ignoreKeydown(ev: KeyboardEvent) {
-        ev.stopPropagation();
-    }
-
     private _valueChanged(ev: CustomEvent): void {
-        if (!this._config || !this.hass) {
-            return;
-        }
-        const target = ev.target! as EditorTarget;
-        const value = target.checked ?? ev.detail.value ?? target.value;
-
-        if (this[`_${target.configValue}`] === value) {
-            return;
-        }
-
-        let newConfig;
-        if (target.configValue) {
-            if (!value) {
-                newConfig = { ...this._config };
-                delete newConfig[target.configValue!];
-            } else {
-                newConfig = {
-                    ...this._config,
-                    [target.configValue!]: value,
-                };
-            }
-        }
-        fireEvent(this, "config-changed", { config: newConfig });
+        fireEvent(this, "config-changed", { config: ev.detail.value });
     }
 
     static get styles(): CSSResultGroup {
