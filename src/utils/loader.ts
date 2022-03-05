054507ce2be0b1bae0da0cb57200494c22fe3103
piitaya <paul.bottein@gmail.com>
2022-03-05 23:23:02 +0100

    chore: change load ha components strategy

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -1,10 +1,9 @@
 // Hack to load ha-components needed for editor
 export const loadHaComponents = () => {
-    if (
-        !customElements.get("hui-action-editor") ||
-        !customElements.get("ha-icon-picker") ||
-        !customElements.get("ha-entity-picker")
-    ) {
+    if (!customElements.get("ha-form")) {
         (customElements.get("hui-button-card") as any)?.getConfigElement();
+    }
+    if (!customElements.get("ha-entity-picker")) {
+        (customElements.get("hui-conditional-card-editor") as any)?.getConfigElement();
     }
 };
