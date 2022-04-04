8c40a6ce63ff5ac8172e319531a6c4c6147a113c
rhuba8324 <53090204+rhuba8324@users.noreply.github.com>
2022-04-04 20:05:22 +0800

    Update localize.ts

--- src/localize.ts
+++ src/localize.ts
@@ -11,7 +11,7 @@ import * as pt_BR from "./translations/pt-BR.json";
 import * as sv from "./translations/sv.json";
 import * as zh_Hans from "./translations/zh-Hans.json";
 import * as nl from "./translations/nl.json";
-import * as zh_TW from "./translations/zh-tw.json";
+import * as zh_Hant from "./translations/zh-Hant.json";
 
 const languages: Record<string, unknown> = {
     de,
@@ -22,7 +22,7 @@ const languages: Record<string, unknown> = {
     sv,
     "zh-Hans": zh_Hans,
     nl,
-    "zh-tw": zh_TW,
+    "zh-Hant": zh_Hant,
 };
 
 const DEFAULT_LANG = "en";
