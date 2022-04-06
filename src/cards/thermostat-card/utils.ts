25cc9db96c3ef9866aeccde25a1132f6f71e2b51
ulic75 <ulic75@gmail.com>
2022-04-05 20:21:43 -0600

    rename param to entity

--- src/cards/thermostat-card/utils.ts
+++ src/cards/thermostat-card/utils.ts
@@ -1,15 +1,14 @@
 import { HassEntity } from "home-assistant-js-websocket";
-import { UNAVAILABLE } from "../../utils/entity";
 import { HVAC_MODE_ORDERING } from "./const";
 import { HvacMode } from "./types";
 
 export const compareClimateHvacModes = (mode1: HvacMode, mode2: HvacMode) =>
     HVAC_MODE_ORDERING[mode1] - HVAC_MODE_ORDERING[mode2];
 
-export const getSetTemp = (stateObj: HassEntity): number | [number, number] => {
-    if (stateObj.attributes.target_temp_low && stateObj.attributes.target_temp_high) {
-        return [stateObj.attributes.target_temp_low, stateObj.attributes.target_temp_high];
+export const getSetTemp = (entity: HassEntity): number | [number, number] => {
+    if (entity.attributes.target_temp_low && entity.attributes.target_temp_high) {
+        return [entity.attributes.target_temp_low, entity.attributes.target_temp_high];
     }
 
-    return stateObj.attributes.temperature;
+    return entity.attributes.temperature;
 };
