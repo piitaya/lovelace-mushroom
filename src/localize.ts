e715a1810ea87989580fd2160d8fd307823c7f48
dsimop <dsimop@users.noreply.github.com>
2022-04-08 14:42:42 +0200

    Update localize.ts

--- src/localize.ts
+++ src/localize.ts
@@ -4,6 +4,7 @@
 import { HomeAssistant } from "custom-card-helpers";
 
 import * as de from "./translations/de.json";
+import * as el from "./translations/el.json";
 import * as en from "./translations/en.json";
 import * as fr from "./translations/fr.json";
 import * as it from "./translations/it.json";
