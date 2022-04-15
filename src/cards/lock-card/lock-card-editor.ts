0dbd5696b6cc9be8b7fce9acbd47b34233366f05
acesyde <acesyde@gmail.com>
2022-04-15 11:07:42 +0200

    feat(card): lock

---
+++ src/cards/lock-card/lock-card-editor.ts
@@ -0,0 +1,91 @@
+import { fireEvent, HomeAssistant, LovelaceCardEditor } from "custom-card-helpers";
+import { CSSResultGroup, html, LitElement, TemplateResult } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import memoizeOne from "memoize-one";
+import { assert } from "superstruct";
+import setupCustomlocalize from "../../localize";
+import { configElementStyle } from "../../utils/editor-styles";
+import { GENERIC_FIELDS } from "../../utils/form/fields";
+import { HaFormSchema } from "../../utils/form/ha-form";
+import { stateIcon } from "../../utils/icons/state-icon";
+import { loadHaComponents } from "../../utils/loader";
+import { LOCK_CARD_EDITOR_NAME } from "./const";
+import { LockCardConfig, lockCardConfigStruct } from "./lock-card-config";
+
+const computeSchema = memoizeOne((icon?: string): HaFormSchema[] => [
+    { name: "entity", selector: { entity: {} } },
+    { name: "name", selector: { text: {} } },
+    {
+        type: "grid",
+        name: "",
+        schema: [
+            { name: "icon", selector: { icon: { placeholder: icon } } },
+            { name: "icon_color", selector: { "mush-color": {} } },
+        ],
+    },
+    {
+        type: "grid",
+        name: "",
+        schema: [
+            { name: "layout", selector: { "mush-layout": {} } },
+        ],
+    },
+    { name: "tap_action", selector: { "mush-action": {} } },
+    { name: "hold_action", selector: { "mush-action": {} } },
+    { name: "double_tap_action", selector: { "mush-action": {} } },
+]);
+
+@customElement(LOCK_CARD_EDITOR_NAME)
+export class LockCardEditor extends LitElement implements LovelaceCardEditor {
+    @property({ attribute: false }) public hass?: HomeAssistant;
+
+    @state() private _config?: LockCardConfig;
+
+    connectedCallback() {
+        super.connectedCallback();
+        void loadHaComponents();
+    }
+
+    public setConfig(config: LockCardConfig): void {
+        assert(config, lockCardConfigStruct);
+        this._config = config;
+    }
+
+    private _computeLabelCallback = (schema: HaFormSchema) => {
+        const customLocalize = setupCustomlocalize(this.hass!);
+
+        if (GENERIC_FIELDS.includes(schema.name)) {
+            return customLocalize(`editor.card.generic.${schema.name}`);
+        }
+        return this.hass!.localize(`ui.panel.lovelace.editor.card.generic.${schema.name}`);
+    };
+
+    protected render(): TemplateResult {
+        if (!this.hass || !this._config) {
+            return html``;
+        }
+
+        const entityState = this._config.entity ? this.hass.states[this._config.entity] : undefined;
+        const entityIcon = entityState ? stateIcon(entityState) : undefined;
+        const icon = this._config.icon || entityIcon;
+        const schema = computeSchema(icon);
+
+        return html`
+            <ha-form
+                .hass=${this.hass}
+                .data=${this._config}
+                .schema=${schema}
+                .computeLabel=${this._computeLabelCallback}
+                @value-changed=${this._valueChanged}
+            ></ha-form>
+        `;
+    }
+
+    private _valueChanged(ev: CustomEvent): void {
+        fireEvent(this, "config-changed", { config: ev.detail.value });
+    }
+
+    static get styles(): CSSResultGroup {
+        return configElementStyle;
+    }
+}
