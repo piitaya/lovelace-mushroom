c171ee140b4d2a92f2f9a982858f2147605b1a18
Greg Hesp <greg.hesp@gmail.com>
2022-04-17 18:35:28 +0100

    Amend login to allow for room presence

--- src/cards/person-card/utils.ts
+++ src/cards/person-card/utils.ts
@@ -5,14 +5,18 @@ export function getStateIcon(entity: HassEntity, zones: HassEntity[]) {
     const state = entity.state;
     if (state === UNKNOWN) {
         return "mdi:help";
+    } else if(state === "not_home") {
+        return "mdi:home-export-outline"
     } else if (state === "home") {
         return "mdi:home";
     }
+
     const zone = zones.find((z) => state === z.attributes.friendly_name);
     if (zone && zone.attributes.icon) {
         return zone.attributes.icon;
     }
-    return "mdi:home-export-outline";
+
+    return "mdi:home";
 }
 
 export function getStateColor(entity: HassEntity, zones: HassEntity[]) {
