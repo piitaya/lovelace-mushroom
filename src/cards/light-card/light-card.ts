129cd2da53dd5de7f3097b58d073a1d7c664a4fd
M4D1NG3R <67385257+M4D1NG3R@users.noreply.github.com>
2022-04-28 19:41:46 +0200

    revert to card takes full height

--- src/cards/light-card/light-card.ts
+++ src/cards/light-card/light-card.ts
@@ -199,61 +199,47 @@ export class LightCard extends LitElement implements LovelaceCard {
 
         const rtl = computeRTL(this.hass);
 
-        const content =  html`
-            <mushroom-state-item
-                ?rtl=${rtl}
-                .layout=${layout}
-                @action=${this._handleAction}
-                .actionHandler=${actionHandler({
-                    hasHold: hasAction(this._config.hold_action),
-                    hasDoubleClick: hasAction(this._config.double_tap_action),
-                })}
-            >
-                <mushroom-shape-icon
-                    slot="icon"
-                    .disabled=${!active}
-                    .icon=${icon}
-                    style=${styleMap(iconStyle)}
-                ></mushroom-shape-icon>
-                ${!isAvailable(entity)
+        return html`
+            <mushroom-card .layout=${layout} ?rtl=${rtl}>
+                <mushroom-state-item
+                    ?rtl=${rtl}
+                    .layout=${layout}
+                    @action=${this._handleAction}
+                    .actionHandler=${actionHandler({
+                        hasHold: hasAction(this._config.hold_action),
+                        hasDoubleClick: hasAction(this._config.double_tap_action),
+                    })}
+                >
+                    <mushroom-shape-icon
+                        slot="icon"
+                        .disabled=${!active}
+                        .icon=${icon}
+                        style=${styleMap(iconStyle)}
+                    ></mushroom-shape-icon>
+                    ${!isAvailable(entity)
+                        ? html`
+                              <mushroom-badge-icon
+                                  class="unavailable"
+                                  slot="badge"
+                                  icon="mdi:help"
+                              ></mushroom-badge-icon>
+                          `
+                        : null}
+                    <mushroom-state-info
+                        slot="info"
+                        .primary=${name}
+                        .secondary=${!hideState && stateValue}
+                    ></mushroom-state-info>
+                </mushroom-state-item>
+                ${this._controls.length > 0
                     ? html`
-                            <mushroom-badge-icon
-                                class="unavailable"
-                                slot="badge"
-                                icon="mdi:help"
-                            ></mushroom-badge-icon>
-                        `
+                          <div class="actions" ?rtl=${rtl}>
+                              ${this.renderActiveControl(entity)} ${this.renderOtherControls()}
+                          </div>
+                      `
                     : null}
-                <mushroom-state-info
-                    slot="info"
-                    .primary=${name}
-                    .secondary=${!hideState && stateValue}
-                ></mushroom-state-info>
-            </mushroom-state-item>
-            ${this._controls.length > 0
-                ? html`
-                        <div class="actions" ?rtl=${rtl}>
-                            ${this.renderActiveControl(entity)} ${this.renderOtherControls()}
-                        </div>
-                    `
-                : null}
+            </mushroom-card>
         `;
-
-        if (this._config.enable_collapse){
-            return html`
-                <ha-card>
-                    <mushroom-card .layout=${layout} no-card-style ?rtl=${rtl}>
-                        ${content}
-                    </mushroom-card>
-                </ha-card>
-            `;
-        }
-        else{
-            return html`
-                <mushroom-card .layout=${layout} ?rtl=${rtl}>
-                ${content}</mushroom-card>
-            `;
-        }
     }
 
     private renderOtherControls(): TemplateResult | null {
