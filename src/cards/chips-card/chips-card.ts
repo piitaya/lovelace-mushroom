ccedbed5036d3aa3123b5579af8eaee089b95f34
Pierre-Emmanuel Mercier <acesyde@gmail.com>
2022-05-17 09:36:33 +0200

    fix: missing ha-card (#482)
    * fix: missing ha-card
    
    * fix: missing ha-card
    
    * fix: styles

--- src/cards/chips-card/chips-card.ts
+++ src/cards/chips-card/chips-card.ts
@@ -80,9 +80,11 @@ export class ChipsCard extends LitElement implements LovelaceCard {
         const rtl = computeRTL(this._hass);
 
         return html`
-            <div class="chip-container ${alignment}" ?rtl=${rtl}>
-                ${this._config.chips.map((chip) => this.renderChip(chip))}
-            </div>
+            <ha-card>
+                <div class="chip-container ${alignment}" ?rtl=${rtl}>
+                    ${this._config.chips.map((chip) => this.renderChip(chip))}
+                </div>
+            </ha-card>
         `;
     }
 
@@ -102,6 +104,11 @@ export class ChipsCard extends LitElement implements LovelaceCard {
             MushroomBaseElement.styles,
             cardStyle,
             css`
+                ha-card {
+                    background: none;
+                    box-shadow: none;
+                    border-radius: 0;
+                }
                 .chip-container {
                     display: flex;
                     flex-direction: row;
