024140912c1fe22000c5e53455499de85f50f503
Miguel Diaz Gonçalves <39262857+mdiazgoncalves@users.noreply.github.com>
2022-05-19 17:50:48 +0100

    Update localize.ts

--- src/localize.ts
+++ src/localize.ts
@@ -12,6 +12,7 @@ import * as nb from "./translations/nb.json";
 import * as nl from "./translations/nl.json";
 import * as pl from "./translations/pl.json";
 import * as pt_BR from "./translations/pt-BR.json";
+import * as pt_PT from "./translations/pt-PT.json";
 import * as sv from "./translations/sv.json";
 import * as tr from "./translations/tr.json";
 import * as vi from "./translations/vi.json";
@@ -29,6 +30,7 @@ const languages: Record<string, unknown> = {
     nl,
     pl,
     "pt-BR": pt_BR,
+    "pt-PT": pt_PT,
     sv,
     tr,
     vi,
