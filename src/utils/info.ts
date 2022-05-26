a8ad3381e99b544a1abe6267e9e7bd398198629d
Andreas Bergander <andreas.bergander@benify.com>
2022-05-26 11:37:55 +0200

    Add a "name & state" option for the content of the entity chips cards

--- src/utils/info.ts
+++ src/utils/info.ts
@@ -3,7 +3,7 @@ import { HassEntity } from "home-assistant-js-websocket";
 import { html } from "lit";
 import { isAvailable, isUnknown } from "../ha/data/entity";
 
-export const INFOS = ["name", "state", "last-changed", "last-updated", "none"] as const;
+export const INFOS = ["name", "state", "name-and-state", "last-changed", "last-updated", "none"] as const;
 const TIMESTAMP_STATE_DOMAINS = ["button", "input_button", "scene"];
 
 export type Info = typeof INFOS[number];
@@ -15,27 +15,33 @@ export function getInfo(
     entity: HassEntity,
     hass: HomeAssistant
 ) {
-    switch (info) {
-        case "name":
-            return name;
-        case "state":
-            const domain = entity.entity_id.split(".")[0];
-            if (
-                (entity.attributes.device_class === "timestamp" ||
-                    TIMESTAMP_STATE_DOMAINS.includes(domain)) &&
-                isAvailable(entity) &&
-                !isUnknown(entity)
-            ) {
-                return html`
+    function getState() {
+        const domain = entity.entity_id.split(".")[0];
+        if (
+            (entity.attributes.device_class === "timestamp" ||
+                TIMESTAMP_STATE_DOMAINS.includes(domain)) &&
+            isAvailable(entity) &&
+            !isUnknown(entity)
+        ) {
+            return html`
                     <ha-relative-time
                         .hass=${hass}
                         .datetime=${entity.state}
                         capitalize
                     ></ha-relative-time>
                 `;
-            } else {
-                return state;
-            }
+        } else {
+            return state;
+        }
+    }
+
+    switch (info) {
+        case "name":
+            return name;
+        case "state":
+            return getState();
+        case "name-and-state":
+            return name + " " + getState();
         case "last-changed":
             return html`
                 <ha-relative-time
