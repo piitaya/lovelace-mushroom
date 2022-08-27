dd6b254d2a22ee455ce3d4f176603aa83765c353
BizzySleepin <bryanrstewart@outlook.com>
2022-08-27 13:24:37 -0700

    fix row types

--- src/utils/lovelace/create-row.ts
+++ src/utils/lovelace/create-row.ts
@@ -1,5 +1,14 @@
-import { HomeAssistant, LovelaceRow } from "../../ha";
+import { HomeAssistant } from "../../ha";
 import { computeRowType } from "../../ha/common/entity/compute-row-type";
+
+export interface LovelaceRow extends HTMLElement {
+    hass?: HomeAssistant;
+    setConfig(config: LovelaceRowConfig): void;
+}
+
+export interface LovelaceRowConfig {
+    entity: string;
+}
 
 export const createRow = (hass: HomeAssistant, entity: string) => {
     const rowType = computeRowType(entity);
