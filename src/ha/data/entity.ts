99d47f85df9d6f5a765433f7db04972e3abed6da
Paul Bottein <paul.bottein@gmail.com>
2022-10-29 09:06:39 +0200

    Improve script, button, scene and media player active state. (#802)

--- src/ha/data/entity.ts
+++ src/ha/data/entity.ts
@@ -1,4 +1,5 @@
 import { HassEntity } from "home-assistant-js-websocket";
+import { computeDomain } from "../common/entity/compute-domain";
 
 export const UNAVAILABLE = "unavailable";
 export const UNKNOWN = "unknown";
@@ -6,10 +7,21 @@ export const UNKNOWN = "unknown";
 export const ON = "on";
 export const OFF = "off";
 
+const OFF_STATES = [UNAVAILABLE, UNKNOWN, OFF];
+const NORMAL_UNKNOWN_DOMAIN = ["button", "input_button", "scene"];
+const NORMAL_OFF_DOMAIN = ["script"];
+
 export function isActive(entity: HassEntity) {
-    const domain = entity.entity_id.split(".")[0];
+    const domain = computeDomain(entity.entity_id);
     const state = entity.state;
-    if (state === UNAVAILABLE || state === UNKNOWN || state === OFF) return false;
+
+    if (
+        OFF_STATES.includes(state) &&
+        !(NORMAL_UNKNOWN_DOMAIN.includes(domain) && state === "unknown") &&
+        !(NORMAL_OFF_DOMAIN.includes(domain) && state === "script")
+    ) {
+        return false;
+    }
 
     // Custom cases
     switch (domain) {
@@ -22,6 +34,8 @@ export function isActive(entity: HassEntity) {
         case "device_tracker":
         case "person":
             return state === "home";
+        case "media-player":
+            return state !== "idle" && state !== "standby";
         case "vacuum":
             return state === "cleaning" || state === "on";
         case "plant":
