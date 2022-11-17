cda3693af473886f23d2186894082c3036155cd5
Răzvan Ștefănescu <razvan.stefanescu@mazars.ro>
2022-11-17 10:36:59 +0200

    Added Romanian translation

--- src/localize.ts
+++ src/localize.ts
@@ -16,6 +16,7 @@ import * as nl from "./translations/nl.json";
 import * as pl from "./translations/pl.json";
 import * as pt_BR from "./translations/pt-BR.json";
 import * as pt_PT from "./translations/pt-PT.json";
+import * as ro from './translations/ro.json';
 import * as sk from './translations/sk.json';
 import * as sv from "./translations/sv.json";
 import * as tr from "./translations/tr.json";
@@ -41,6 +42,7 @@ const languages: Record<string, unknown> = {
     pl,
     "pt-BR": pt_BR,
     "pt-PT": pt_PT,
+    ro,
     sk,
     sv,
     tr,
