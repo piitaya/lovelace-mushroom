8690be0f4a50378e497e603b9a338cc3f1f9f3b3
TravisWilder <60007888+TravisWilder@users.noreply.github.com>
2023-02-03 16:44:23 +0100

    Update utils.ts

--- src/cards/person-card/utils.ts
+++ src/cards/person-card/utils.ts
@@ -5,12 +5,15 @@ export function getStateIcon(entity: HassEntity, zones: HassEntity[]) {
     const state = entity.state;
     if (state === UNKNOWN) {
         return "mdi:help";
-    } else if (state === "not_home" || state === "Stationary") {
+    } else if (state === "not_home") {
         return "mdi:home-export-outline";
     } else if (state === "home") {
         return "mdi:home";
+    }  else {
+        return "mdi:map-marker-radius";
     }
-
+ 
+    
     const zone = zones.find((z) => state === z.attributes.friendly_name);
     if (zone && zone.attributes.icon) {
         return zone.attributes.icon;
@@ -23,10 +26,12 @@ export function getStateColor(entity: HassEntity, zones: HassEntity[]) {
     const state = entity.state;
     if (state === UNKNOWN) {
         return "var(--rgb-state-person-unknown)";
-    } else if (state === "not_home" || state === "Stationary") {
+    } else if (state === "not_home") {
         return "var(--rgb-state-person-not-home)";
     } else if (state === "home") {
         return "var(--rgb-state-person-home)";
+    } else {
+        return "var(--rgb-state-person-not-home)";;
     }
     const isInZone = zones.some((z) => state === z.attributes.friendly_name);
     if (isInZone) {
