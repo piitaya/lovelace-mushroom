9e3c8fce0e2758b592a2deebf239144570f2b56e
Paul Bottein <paul.bottein@gmail.com>
2023-04-25 22:55:36 +0200

    Create not found card

--- src/utils/base-card.ts
+++ src/utils/base-card.ts
@@ -1,15 +1,21 @@
 import { HassEntity } from "home-assistant-js-websocket";
 import { html, nothing, TemplateResult } from "lit";
-import { computeStateDisplay, HomeAssistant, isActive, isAvailable } from "../ha";
+import { classMap } from "lit/directives/class-map.js";
+import { computeRTL, computeStateDisplay, HomeAssistant, isActive, isAvailable } from "../ha";
+import setupCustomlocalize from "../localize";
 import "../shared/badge-icon";
 import "../shared/card";
-import { Appearance } from "../shared/config/appearance-config";
+import { Appearance, AppearanceSharedConfig } from "../shared/config/appearance-config";
+import { EntitySharedConfig } from "../shared/config/entity-config";
 import "../shared/shape-avatar";
 import "../shared/shape-icon";
 import "../shared/state-info";
 import "../shared/state-item";
+import { computeAppearance } from "./appearance";
 import { MushroomBaseElement } from "./base-element";
 import { computeInfoDisplay } from "./info";
+
+type BaseConfig = EntitySharedConfig & AppearanceSharedConfig;
 
 export function computeDarkMode(hass?: HomeAssistant): boolean {
     if (!hass) return false;
@@ -22,6 +28,37 @@ export class MushroomBaseCard extends MushroomBaseElement {
                 slot="icon"
                 .picture_url=${(this.hass as any).hassUrl(picture)}
             ></mushroom-shape-avatar>
+        `;
+    }
+
+    protected renderNotFound(config: BaseConfig): TemplateResult {
+        const appearance = computeAppearance(config);
+        const rtl = computeRTL(this.hass);
+
+        const customLocalize = setupCustomlocalize(this.hass);
+
+        return html`
+            <ha-card class=${classMap({ "fill-container": appearance.fill_container })}>
+                <mushroom-card .appearance=${appearance} ?rtl=${rtl}>
+                    <mushroom-state-item ?rtl=${rtl} .appearance=${appearance} disabled>
+                        <mushroom-shape-icon
+                            slot="icon"
+                            disabled
+                            icon="mdi:help"
+                        ></mushroom-shape-icon>
+                        <mushroom-badge-icon
+                            slot="badge"
+                            class="not-found"
+                            icon="mdi:exclamation-thick"
+                        ></mushroom-badge-icon>
+                        <mushroom-state-info
+                            slot="info"
+                            .primary=${config.entity}
+                            secondary=${customLocalize("card.not_found")}
+                        ></mushroom-state-info>
+                    </mushroom-state-item>
+                </mushroom-card>
+            </ha-card>
         `;
     }
 
