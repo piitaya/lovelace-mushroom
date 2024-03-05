fd478cc63d5886c882f4e2ff080c97aea1331647
Paul Bottein <paul.bottein@gmail.com>
2024-03-05 15:16:02 +0100

    Add grid support to number card

--- src/cards/number-card/number-card.ts
+++ src/cards/number-card/number-card.ts
@@ -42,7 +42,7 @@ registerCustomCard({
 });
 
 @customElement(NUMBER_CARD_NAME)
-export class NumberCard extends MushroomBaseCard implements LovelaceCard {
+export class NumberCard extends MushroomBaseCard<NumberCardConfig> implements LovelaceCard {
     public static async getConfigElement(): Promise<LovelaceCardEditor> {
         await import("./number-card-editor");
         return document.createElement(NUMBER_CARD_EDITOR_NAME) as LovelaceCardEditor;
@@ -57,25 +57,11 @@ export class NumberCard extends MushroomBaseCard implements LovelaceCard {
         };
     }
 
-    @state() private _config?: NumberCardConfig;
+    protected get hasControls(): boolean {
+        return true;
+    }
 
     @state() private value?: number;
-
-    getCardSize(): number | Promise<number> {
-        return 1;
-    }
-
-    setConfig(config: NumberCardConfig): void {
-        this._config = {
-            tap_action: {
-                action: "more-info",
-            },
-            hold_action: {
-                action: "more-info",
-            },
-            ...config,
-        };
-    }
 
     private _handleAction(ev: ActionHandlerEvent) {
         handleAction(this, this.hass!, this._config!, ev.detail.action!);
