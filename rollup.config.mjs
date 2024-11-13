9d4780bbd063ed2916821b155634644899cb8b4e
Paul Bottein <paul.bottein@gmail.com>
2024-11-13 12:51:34 +0100

    Add better support for old browsers

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,7 @@
-import babel from "@rollup/plugin-babel";
+import {
+  getBabelInputPlugin,
+  getBabelOutputPlugin,
+} from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
@@ -8,60 +11,71 @@ import serve from "rollup-plugin-serve";
 import ignore from "./rollup-plugins/rollup-ignore-plugin.js";
 
 const IGNORED_FILES = [
-    "@material/mwc-notched-outline/mwc-notched-outline.js",
-    "@material/mwc-ripple/mwc-ripple.js",
-    "@material/mwc-list/mwc-list.js",
-    "@material/mwc-list/mwc-list-item.js",
-    "@material/mwc-menu/mwc-menu.js",
-    "@material/mwc-menu/mwc-menu-surface.js",
-    "@material/mwc-icon/mwc-icon.js",
+  "@material/mwc-notched-outline/mwc-notched-outline.js",
+  "@material/mwc-ripple/mwc-ripple.js",
+  "@material/mwc-list/mwc-list.js",
+  "@material/mwc-list/mwc-list-item.js",
+  "@material/mwc-menu/mwc-menu.js",
+  "@material/mwc-menu/mwc-menu-surface.js",
+  "@material/mwc-icon/mwc-icon.js",
 ];
 
 const dev = process.env.ROLLUP_WATCH;
 
 const serveOptions = {
-    contentBase: ["./dist"],
-    host: "0.0.0.0",
-    port: 4000,
-    allowCrossOrigin: true,
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-    },
+  contentBase: ["./dist"],
+  host: "0.0.0.0",
+  port: 4000,
+  allowCrossOrigin: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
 };
 
 const plugins = [
-    ignore({
-        files: IGNORED_FILES.map((file) => require.resolve(file)),
-    }),
-    typescript({
-        declaration: false,
-    }),
-    nodeResolve(),
-    json(),
-    commonjs(),
-    babel({
-        babelHelpers: "bundled",
-    }),
-    ...(dev ? [serve(serveOptions)] : [terser()]),
+  ignore({
+    files: IGNORED_FILES.map((file) => require.resolve(file)),
+  }),
+  typescript({
+    declaration: false,
+  }),
+  nodeResolve(),
+  json(),
+  commonjs(),
+  getBabelInputPlugin({
+    babelHelpers: "bundled",
+  }),
+  getBabelOutputPlugin({
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          modules: false,
+        },
+      ],
+    ],
+    compact: true,
+  }),
+  ...(dev ? [serve(serveOptions)] : [terser()]),
 ];
 
 export default [
-    {
-        input: "src/mushroom.ts",
-        output: {
-            dir: "dist",
-            format: "es",
-            inlineDynamicImports: true,
-        },
-        plugins,
-        moduleContext: (id) => {
-            const thisAsWindowForModules = [
-                "node_modules/@formatjs/intl-utils/lib/src/diff.js",
-                "node_modules/@formatjs/intl-utils/lib/src/resolve-locale.js",
-            ];
-            if (thisAsWindowForModules.some((id_) => id.trimRight().endsWith(id_))) {
-                return "window";
-            }
-        },
+  {
+    input: "src/mushroom.ts",
+    output: {
+      dir: "dist",
+      format: "es",
+      inlineDynamicImports: true,
     },
+    plugins,
+    moduleContext: (id) => {
+      const thisAsWindowForModules = [
+        "node_modules/@formatjs/intl-utils/lib/src/diff.js",
+        "node_modules/@formatjs/intl-utils/lib/src/resolve-locale.js",
+      ];
+      if (thisAsWindowForModules.some((id_) => id.trimRight().endsWith(id_))) {
+        return "window";
+      }
+    },
+  },
 ];
