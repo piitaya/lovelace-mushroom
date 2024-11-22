d1fd163705eaeddebc4a98a1c717ae1164382489
Paul Bottein <paul.bottein@gmail.com>
2024-11-22 17:48:48 +0100

    Fix number duration format

--- src/cards/number-card/number-card.ts
+++ src/cards/number-card/number-card.ts
@@ -14,9 +14,6 @@ import {
   actionHandler,
   ActionHandlerEvent,
   computeRTL,
-  formatNumber,
-  getDefaultFormatOptions,
-  getNumberFormatOptions,
   handleAction,
   hasAction,
   HomeAssistant,
@@ -125,15 +122,10 @@ export class NumberCard
 
     let stateDisplay = this.hass.formatEntityState(stateObj);
     if (this.value !== undefined) {
-      const numberValue = formatNumber(
-        this.value,
-        this.hass.locale,
-        getNumberFormatOptions(
-          stateObj,
-          this.hass.entities[stateObj.entity_id]
-        ) ?? getDefaultFormatOptions(stateObj.state)
+      stateDisplay = this.hass.formatEntityState(
+        stateObj,
+        this.value.toString()
       );
-      stateDisplay = `${numberValue} ${stateObj.attributes.unit_of_measurement ?? ""}`;
     }
 
     const rtl = computeRTL(this.hass);
