b475e98311d52069c2b53f55329efc8b205325cc
Tony Brüser <113691123+404GamerNotFound@users.noreply.github.com>
2026-04-19 06:48:27 +0200

    Add Mushroom input datetime card

---
+++ src/cards/input-datetime-card/const.ts
@@ -0,0 +1,5 @@
+import { PREFIX_NAME } from "../../const";
+
+export const INPUT_DATETIME_CARD_NAME = `${PREFIX_NAME}-input-datetime-card`;
+export const INPUT_DATETIME_CARD_EDITOR_NAME = `${INPUT_DATETIME_CARD_NAME}-editor`;
+export const INPUT_DATETIME_ENTITY_DOMAINS = ["input_datetime"];
---
+++ src/cards/input-datetime-card/input-datetime-card-editor.ts
@@ -0,0 +1,95 @@
+import { html, nothing } from "lit";
+import { customElement, state } from "lit/decorators.js";
+import memoizeOne from "memoize-one";
+import { assert } from "superstruct";
+import { LocalizeFunc, LovelaceCardEditor, fireEvent } from "../../ha";
+import setupCustomlocalize from "../../localize";
+import { computeActionsFormSchema } from "../../shared/config/actions-config";
+import { computeAppearanceFormSchema } from "../../shared/config/appearance-config";
+import { MushroomBaseElement } from "../../utils/base-element";
+import { GENERIC_LABELS } from "../../utils/form/generic-fields";
+import { HaFormSchema } from "../../utils/form/ha-form";
+import { loadHaComponents } from "../../utils/loader";
+import {
+  INPUT_DATETIME_CARD_EDITOR_NAME,
+  INPUT_DATETIME_ENTITY_DOMAINS,
+} from "./const";
+import {
+  InputDatetimeCardConfig,
+  inputDatetimeCardConfigStruct,
+} from "./input-datetime-card-config";
+
+const computeSchema = memoizeOne((localize: LocalizeFunc): HaFormSchema[] => [
+  {
+    name: "entity",
+    selector: { entity: { domain: INPUT_DATETIME_ENTITY_DOMAINS } },
+  },
+  { name: "name", selector: { text: {} } },
+  {
+    type: "grid",
+    name: "",
+    schema: [
+      {
+        name: "icon",
+        selector: { icon: {} },
+        context: { icon_entity: "entity" },
+      },
+      { name: "icon_color", selector: { ui_color: {} } },
+    ],
+  },
+  ...computeAppearanceFormSchema(localize),
+  ...computeActionsFormSchema(),
+]);
+
+@customElement(INPUT_DATETIME_CARD_EDITOR_NAME)
+export class InputDatetimeCardEditor
+  extends MushroomBaseElement
+  implements LovelaceCardEditor
+{
+  @state() private _config?: InputDatetimeCardConfig;
+
+  connectedCallback() {
+    super.connectedCallback();
+    void loadHaComponents();
+  }
+
+  public setConfig(config: InputDatetimeCardConfig): void {
+    assert(config, inputDatetimeCardConfigStruct);
+    this._config = config;
+  }
+
+  private _computeLabel = (schema: HaFormSchema) => {
+    const customLocalize = setupCustomlocalize(this.hass!);
+
+    if (GENERIC_LABELS.includes(schema.name)) {
+      return customLocalize(`editor.card.generic.${schema.name}`);
+    }
+
+    return this.hass!.localize(
+      `ui.panel.lovelace.editor.card.generic.${schema.name}`
+    );
+  };
+
+  protected render() {
+    if (!this.hass || !this._config) {
+      return nothing;
+    }
+
+    const customLocalize = setupCustomlocalize(this.hass);
+    const schema = computeSchema(customLocalize);
+
+    return html`
+      <ha-form
+        .hass=${this.hass}
+        .data=${this._config}
+        .schema=${schema}
+        .computeLabel=${this._computeLabel}
+        @value-changed=${this._valueChanged}
+      ></ha-form>
+    `;
+  }
+
+  private _valueChanged(ev: CustomEvent): void {
+    fireEvent(this, "config-changed", { config: ev.detail.value });
+  }
+}
